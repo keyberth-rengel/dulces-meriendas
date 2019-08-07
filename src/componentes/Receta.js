b116f9e468230be6682734d74d6dce3ad6f5ef8c
keyberthRengel <keyrengel24@gmail.com>
2019-08-07 21:22:42 +0200

    agregando key a los componentes y mostrando preparacion adicional

--- src/componentes/Receta.js
+++ src/componentes/Receta.js
@@ -1,23 +1,22 @@
 import React, {Component} from 'react';
 import {Link} from 'react-router-dom';
+import uuid from 'uuid';
 
 class Receta extends Component {
 
     mostrarReceta = () => {
         const receta = this.props.receta;
-
         this.props.mostrarReceta(receta);
     }
     render() {
-        const { img, titulo, descripcion, id } = this.props.receta;
+        const { img, titulo, descripcion } = this.props.receta;
         return (
-            <Link to={`recetas/${id}`} onClick={ this.mostrarReceta } className="col-12 d-flex mb-1 p-2 div-receta">
+            <Link to={`recetas/${uuid()}`} onClick={ this.mostrarReceta } className="col-12 d-flex mb-1 p-2 div-receta">
                 <div className="col-4 aling-item-center contenedor-img">
                     <img className="img-receta" src={img} alt={titulo} />
                 </div>
                 <div className="col-8">
                     <h3>{titulo}</h3>
-                    {/* <p>Categoria: {categoria}</p> */}
                     <p>  {descripcion}</p>
                 </div>
             </Link>
--- src/componentes/View.js
+++ src/componentes/View.js
@@ -2,32 +2,44 @@ import React, {Component} from 'react';
 
 class View extends Component {
     render() {
-        const { img, titulo, ingredientes, preparacion, categoria } = this.props.view;
+        const { img, titulo, ingredientes, preparacion, categoria, crema = [], adicional = "" } = this.props.view;
+        
         return (
            <div className="row m-3 ">
                <div className="col-12 col-md-8 col-lg-8 ">
                     <h2>{titulo}</h2>
-                    <p>Categoria: {categoria}</p>
-                    <div className="col-12 mb-2">
-                        <p>Ingredientes:</p>
+                    <p><strong>Categoria:</strong> {categoria}</p>
+                    <div className="col-12 mb-2 p-3">
+                        <p><strong>Ingredientes:</strong></p>
                         {
-                            ingredientes.map( index => (
-                                <li>{index}</li>
+                            ingredientes.map( (index,posicion)  => (
+                                
+                                <li key={posicion}>{index}</li>
+
+                             ) )
+                        }
+                    </div>
+                    <div className="col-12 mt-2 p-3 preparacion">
+                        <p><strong>Preparacion:</strong></p>
+                        {
+                            preparacion.map( (index,po) => (
+
+                                <li key={po} >{index}</li>
                                 
                              ) )
                         }
                     </div>
-                    <div className="col-12">
-                        <p>Preparacion:</p>
+                    <div className="col-12 mt-2 p-3">
+                    <p><strong>{adicional}</strong></p>
                         {
-                            preparacion.map( index => (
-                                <li>{index}</li>
+                            crema.map( (index,po) => (
+
+                                <li key={po} >{index}</li>
                                 
                              ) )
                         }
                     </div>
                     
-
                </div>
                <div className="col-12 col-md-4 col-lg-4">
                     <img className="img-completa pt-4" src={`../${img}`} alt={titulo} />
